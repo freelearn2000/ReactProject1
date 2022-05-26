c30885f055551a70a0368abb65df880204142505
Parvathy Hari <parvathyvhari@gmail.com>
2022-05-26 13:59:57 +0530

    Added axios and displays error message and loading

--- src/pages/Parvathy/Parvathy.tsx
+++ src/pages/Parvathy/Parvathy.tsx
@@ -1,11 +1,62 @@
-import React, { Component } from 'react';
+import { Component } from "react";
+import axios from 'axios';
 
-export class Parvathy extends Component {
-    render() {
-        return (
-            <div>
-                <h1>This Component is done by Parvathy</h1>
-            </div>
-        );
-    }
+
+interface IProps {
+
 }
+interface IState {
+    Loading: boolean,
+    news: {}[] | null,
+    error: { message: string } | null;
+
+}
+export class Parvathy extends Component<IProps, IState> {
+
+    state = { name: `Parvathy`, Loading: true, news: null, error: null };
+
+    // initialization
+    componentDidMount() {
+
+        // Intitiate API call from here
+        axios.get('https://jsonplaceholder.typicode.com/postss')
+            .then(response => {
+                // console.log('Success data :', response.data);
+                this.setState({ Loading: false, news: response.data, error: null });
+            })
+            .catch(error => {
+                // console.log('Error :', error);
+                this.setState({ Loading: false, news: null, error: error });
+            })
+    }
+
+    renderLoading() {
+        const loadingJSX = <h4>Loading....</h4>
+        return loadingJSX;
+    }
+
+    renderError() {
+        const message = this.state.error ? this.state.error['message'] : '';
+        const errorJSX =
+            <div>
+                <h2>This is {this.state.name}'s Component</h2>
+                <h4>{message}</h4>
+            </div>
+        return errorJSX;
+    }
+
+    renderNews() {
+        const newsJSX = <h4>List all data here....</h4>
+        return newsJSX;
+    }
+
+    render() {
+        if (this.state.Loading) {
+            return this.renderLoading();
+        } else if (this.state.error) {
+            return this.renderError();
+        } else {
+            return this.renderNews();
+        }
+    }
+} 
