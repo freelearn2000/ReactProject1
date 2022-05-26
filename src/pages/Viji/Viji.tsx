42a12f2b5ff7acfda9f16ed2f3d9191eac8aade4
viji <vijiblitzz@gmail.com>
2022-05-26 16:22:55 +0530

    Added axios and display error message

--- src/pages/Viji/Viji.tsx
+++ src/pages/Viji/Viji.tsx
@@ -1,15 +1,70 @@
 import { Component } from "react";
+import axios from 'axios';
 
+interface IState {
+    Loading: boolean, 
+    news: {} [] | null, 
+    error: {message: string} | null;
 
-export class Viji extends Component {
+}
 
-    state = {data: `Welcome to the world of React!`};
+interface IProps {
+}
 
-    render( ) {
+export class Viji extends Component <IProps, IState> {
+
+    //state = {data: `Welcome to the world of React!`};
+    state = {Loading: true, news: null, error: null};
+
+    componentDidMount( ) {
+
+        // Intitiate API call from here
+        axios.get('https://jsonplaceholder.typicode.com/postss')
+            .then(response => {
+                console.log('Success data :', response.data);
+                this.setState( {Loading: false, news: response.data, error: null} );
+            })
+            .catch(error => {
+                const message = this.state.error? this.state.error['message'] : 
+                this.setState({Loading: false, news: null, error: error});
+            })               
+    }
+        
+            renderLoading( ) {
+                const loadingJSX = <h4>Loading....</h4>
+                return loadingJSX;
+        
+            }
+        
+            renderError( ) {
+                const message = this.state.error? this.state.error['message'] : '';
+                const errorJSX =
+                <div>
+                    <h2>This is Viji's Component</h2>
+                    <h4>{ message }</h4>
+                </div> 
+                return errorJSX;
+            }
+        
+            renderNews( ) {
+                const newsJSX = <h4>List all news here..</h4>
+                return newsJSX;
+            }
+        
+            render( ) {
+                if ( this.state.Loading) {
+                    return this.renderLoading( );
+                } else if ( this.state.error) {
+                    return this.renderError( );
+                } else {
+                    return this.renderNews( );
+                }
+            }
+   /* render( ) {
         return (
             <div className="ui form">
               <h3>{this.state.data}</h3>       
             </div>
         );
-    }
+    }*/
 } 
