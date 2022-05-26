90bc6584094f5296b28eebe0929d39f4a51b023b
Santhosh <ntasanthosh@gmail.com>
2022-05-26 07:44:33 +0530

    Added axios and displays Error message and Loading

--- src/pages/Santhosh/Santhosh.tsx
+++ src/pages/Santhosh/Santhosh.tsx
@@ -1,14 +1,62 @@
 import { Component } from "react";
+import axios, { responseEncoding } from 'axios';
+import { Interface } from "readline";
 
-export class Santhosh extends Component {
 
-    state = {data: 'This component is done by santhosh.'};
+interface IState {
+    Loading: boolean, 
+    news: {} [] | null, 
+    error: {message: string} | null;
+
+}
+
+interface IProps {
+}
+
+
+export class Santhosh extends Component<IProps, IState> {
+
+    state = {Loading: true, news: null, error: null};
+
+
+    componentDidMount( ) {
+
+        // Intitiate API call from here
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log('Success data :', response.data);
+                this.setState( {Loading: false, news: response.data, error: null} );
+            })
+            .catch(error => {
+                const message = this.state.error? this.state.error['message'] : 
+                this.setState({Loading: false, news: null, error: error});
+            })               
+    }
+    
+    renderLoading( ) {
+        const loadingJSX = <h4>Loading....</h4>
+        return loadingJSX;
+
+    }
+
+    renderError( ) {
+        const message = this.state.error? this.state.error['message'] : '';
+        const errorJSX = <h4>{ message }</h4>
+        return errorJSX;
+    }
+
+    renderNews( ) {
+        const newsJSX = <h4>List all news here....</h4>
+        return newsJSX;
+    }
 
     render( ) {
-        return (
-            <div className="ui form">
-              <h1>{this.state.data}</h1>       
-            </div>
-        );
+        if ( this.state.Loading) {
+            return this.renderLoading( );
+        } else if ( this.state.error) {
+            return this.renderError( );
+        } else {
+            return this.renderNews( );
+        }
     }
 } 
