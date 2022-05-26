b9842cee120a805959ae514432146cef057cf6f9
Divyao <94785328+Divyao@users.noreply.github.com>
2022-05-26 13:14:15 +0530

    feat:Added axios and displays the response

--- src/pages/Divya/Divya.tsx
+++ src/pages/Divya/Divya.tsx
@@ -1,15 +1,74 @@
 import  { Component } from 'react'
+import	axios from 'axios';
 
 
-export class Divya extends Component {
+interface IProps {
 
-    state = {name: "Divya"};
+}
+interface IState {
+	loading: boolean;
+	project: [] | null;
+	error: {message: string} | null;
+
+}
+export class Divya extends Component<IProps, IState> {
+
+    state = {name: 'Divya', loading: true, project: null, error: null};
+
+	componentDidMount( ) {
+		// initiage API Calls from here
+		axios.get(`https://jsonplaceholder.typicode.com/postss`)
+		.then(response => {
+			//console.log(`Success Data: `, response.data);
+			this.setState( {loading: false, project: response.data, error: null} )
+		})
+		.catch(error =>{
+			//console.log(`Error: `, error);
+			this.setState( {loading: false, project: null, error: error} )
+		})
+
+        
+    }
+
+     componentDidUpdate( ) {
+        //console.log('componentDidUpdate')
+     }
+
+     componentWillUnmount( ) {
+        //console.log('componentWillUnmount')
+     }
+
+	renderLoading( ) {
+		const loadingJSX = <h4>Loading..</h4> 
+		return loadingJSX;
+	}
+
+	renderError( ) {
+		const message = this.state.error? this.state.error[`message`] : '';
+		const errorJSX = <div>
+			<h2>{this.state.name}'s Component</h2>
+			<h4>{message}</h4>
+			</div>
+		
+		return errorJSX;
+	}
+
+	renderProject( ) {
+		const projectJSX = <div>
+			<h2>{this.state.name}'s Component</h2>
+			<h4>List all projects here</h4>
+			</div>
+		
+		return projectJSX;
+	}
    
 	render( ) {
-		return (
-			<div>
-				<h1>Welcome {this.state.name} !</h1>
-		    </div>		
-		);
+		if ( this.state.loading ) {
+			return this.renderLoading( );
+		} else if ( this.state.error ) {
+			return this.renderError( );
+		} else {
+			return this.renderProject( );
+		}
 	}	
 }
