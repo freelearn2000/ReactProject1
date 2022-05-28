2fd185cfda39daae702439439cb8bd7e17455a18
AnusreeMohan <anusreemohan@gmail.com>
2022-05-28 16:54:23 +0530

    fixed broken menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -6,7 +6,8 @@ export const Menu = ( props: any ) => {
     // console.log(location.pathname);
 
     return (
-        <div className="ui twelve item menu">
+        <>
+        <div className="ui eight item menu">
             { (location.pathname.endsWith(`/`))?
                     <Link to='/' className="active item">Home</Link>
                 :   <Link to='/' className="item">Home</Link>}
@@ -17,20 +18,22 @@ export const Menu = ( props: any ) => {
                     <Link to='/about' className="active item">About</Link>
                 :   <Link to='/about' className="item">About</Link>}
             { (location.pathname.includes(`/anusreemohan`))?
-                    <Link to='/anusreemohan' className="active item">AnusreeMohan</Link>
-                :   <Link to='/anusreemohan' className="item">AnusreeMohan</Link>}
+                    <Link to='/anusree' className="active item">Anusree</Link>
+                :   <Link to='/anusree' className="item">Anusree</Link>}
             { (location.pathname.includes(`/rakhiravi`))?
-                <Link to='/rakhiravi' className="active item">RakhiRavi</Link>
-            :   <Link to='/rakhiravi' className="item">RakhiRavi</Link>}
+                <Link to='/rakhi' className="active item">Rakhi</Link>
+            :   <Link to='/rakhi' className="item">Rakhi</Link>}
             { (location.pathname.includes('/Santhosh'))?
                 <Link to='/santhosh' className="active item">Santhosh</Link>
             :   <Link to='/santhosh' className="item">Santhosh</Link> }
-            { (location.pathname.includes('/radhika'))?
+             { (location.pathname.includes('/radhika'))?
                <Link to='/radhika' className="active item">Radhika</Link>
             :  <Link to='/radhika' className="item">Radhika</Link>}
             { (location.pathname.includes('/samarakp'))?
                <Link to='/samarakp' className="active item">Samara KP</Link>
             :  <Link to='/samarakp' className="item">Samara KP</Link>}
+        </div>
+        <div className="ui eight item menu">
             { (location.pathname.includes(`/parvathy`))?
                     <Link to='/parvathy' className="active item">Parvathy</Link>
                 :   <Link to='/parvathy' className="item">Parvathy</Link>}
@@ -42,7 +45,20 @@ export const Menu = ( props: any ) => {
                 :   <Link to='/divya' className="item">Divya</Link>}
              { (location.pathname.includes('/bindu'))?
                     <Link to='/bindu' className="active item">Bindu</Link>
-                :   <Link to='/bindu' className="item">Bindu</Link>}    
-        </div>
+                :   <Link to='/bindu' className="item">Bindu</Link>}
+            { (location.pathname.includes(`/dhanya`))?
+                    <Link to='/dhanya' className="active item">Dhanya</Link>
+                :   <Link to='/dhanya' className="item">Dhanya</Link>}
+             { (location.pathname.includes('/bindu'))?
+                    <Link to='/viji' className="active item">Viji</Link>
+                :   <Link to='/viji' className="item">Viji</Link>}  
+            { (location.pathname.includes('/manooja'))?
+                    <Link to='/manooja' className="active item">Manooja</Link>
+                :   <Link to='/manooja' className="item">Manooja</Link>}
+             { (location.pathname.includes('/aiswarya'))?
+                    <Link to='/aiswarya' className="active item">Aiswarya</Link>
+                :   <Link to='/aiswarya' className="item">Aiswarya</Link>}    
+        </div> 
+        </>
     );
 }
