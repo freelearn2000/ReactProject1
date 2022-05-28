aff1af79692d2f3ed1b54cd80a65827132d5b8e2
Rakhi Ravi <rakhiravi19@gmail.com>
2022-05-28 12:27:37 +0530

    Implemented success condition, injected title as props and fixed the alignment issues

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ class App extends Component {
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/anusree" element={<Anusree/>}/>
         <Route path='/santhosh' element={<Santhosh/>} />
-        <Route path="/rakhi" element={<Rakhi/>}/>
+        <Route path="/rakhi" element={<Rakhi title={`This is Rakhi's Component.`}/>}/>
         <Route path="/radhika" element={<Radhika/>}/>
         <Route path="/samara" element={<Samara/>}/>
         <Route path="/parvathy" element={<Parvathy/>}/>
