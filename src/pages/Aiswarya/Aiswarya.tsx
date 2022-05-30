f37fdfd80d47bd1ea8559d65a1087baac1aa926e
AiswaryaML <aiswarya.ml93@gmail.com>
2022-05-30 19:58:54 +0530

    Added Home Page Link

--- src/pages/Aiswarya/Aiswarya.tsx
+++ src/pages/Aiswarya/Aiswarya.tsx
@@ -1,11 +1,10 @@
-import { Component, ReactNode } from "react";
+  import { Component, ReactNode } from "react";
 import axios from "axios";
-
+import { Link } from "react-router-dom";
 
 interface IProps {
     title: any;
 }
-
 
 interface IState {
     loading: boolean,
@@ -15,72 +14,69 @@ interface IState {
 
 export class Aiswarya extends Component <IProps, IState> {
 
-    state = {loading: true, users: null, error: null};
+    state = { loading: true, users: null, error: null };
 
     componentDidMount ( ) {
-
         axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(response => {
-            //console.log('Data :', response.data);
-            this.setState( { loading: false, users: response.data, error: null} );
-        })
-        .catch(error => {
-            //console.log("Data :", error)
-            this.setState( { loading: false, users: null, error: error} );
-        })
-        
-    }
-    
-        renderLoading( ) {
-            const loadingJSX = 
+            .then(response => {
+                //console.log('Data :', response.data);
+                this.setState( { loading: false, users: response.data, error: null} );
+            })
+            .catch(error => {
+                //console.log("Data :", error)
+                this.setState( { loading: false, users: null, error: error} );
+            })  
+        }
 
-            <div className="ui active inverted dimmer">
-                <div className="ui text loader">Loading User Data..!!</div>
-            </div>
-            
+        renderLoading( ) {
+
+            const loadingJSX = 
+                <div className="ui active inverted dimmer">
+                    <div className="ui text loader">Loading User Data..!!</div>
+                </div>  
             return loadingJSX;
         }
 
         rendererror( ) {
+
             const message = this.state.error? this.state.error['message'] : '';
             const errorJSX = 
-            <div>
-                <h4>{ message }</h4>
-            </div>
+                <div>
+                    <h4>{ message }</h4>
+                </div>
             return errorJSX;
+
         }
         renderUserdata( ) {
            
             const  users = this.state.users ? this.state.users : [ ];
             const DataJSX = users.map( (user: {name:number, email:string,id:number,phone:number} ) => {
                 return(
-                //<div className="ui blue text">
-                    <div className="ui grey segment" key={user.id + 'a' } >
-                        <h4  key={user.id + 'b'}><b>Name : </b>{user.name}</h4>
-                        <p key={user.id + 'c'}><b>Email : </b>{user.email}</p>
-                         <p key={user.id + 'd'}><b>Contact No : </b>{user.phone}</p>
+                    <div className="ui segment" key={user.id + 'a' } >
+                        <h4 ><b>Name :</b>{user.name}</h4>
+                        <p><b>Email :</b>{user.email}</p>
+                        <p><b>Contact No :</b>{user.phone}</p>
                      </div>
-                // </div>
-            
-            )
-         });
+                )
+            });
+        return DataJSX;
+        }
 
-            return DataJSX;
-        }
         render( ) {
-            return (
-                <div className="ui inverted grey segment">
-                    <div>
-                        <h2 className ="ui  inverted header"></h2>
-                        <h2 className=" ui center aligned header">{this.props.title}</h2>
-                        {
-                            this.state.loading?this.renderLoading( ):
-                            this.state.users?<>{this.renderUserdata( )}</> :
-                            <><h2> Error Data !!!!</h2>{this.rendererror( )}</>
-                        }
-                    </div>
+            return (  
+                <div>
+                    <h2 className ="ui  inverted header"></h2>
+                    <h2 className=" ui center aligned header">{this.props.title}</h2>
+                        <br/>
+                        <Link to='/*' className="item">Go to Home Page</Link>
+                        <br/>
+                    {
+                        this.state.loading?this.renderLoading( ):
+                        this.state.users?<>{this.renderUserdata( )}</> :
+                        <><h2> Error Data !!!!</h2>{this.rendererror( )}</>
+                    }
                 </div>
-            )
-            
-            }
+            ) 
         }
+
+    }
