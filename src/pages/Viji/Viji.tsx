21c26ebb4660f4d25a9e49f1b10f1e33ff11ca2d
viji <vijiblitzz@gmail.com>
2022-05-30 14:51:03 +0530

    Added user details

--- src/pages/Viji/Viji.tsx
+++ src/pages/Viji/Viji.tsx
@@ -3,30 +3,31 @@ import axios from 'axios';
 
 interface IState {
     Loading: boolean, 
-    news: {} [] | null, 
+    users : {} [] | null, 
     error: {message: string} | null;
 
 }
 
 interface IProps {
+    title: string;
 }
 
 export class Viji extends Component <IProps, IState> {
 
     //state = {data: `Welcome to the world of React!`};
-    state = {Loading: true, news: null, error: null};
+    state = {Loading: true, users: null, error: null};
 
     componentDidMount( ) {
 
         // Intitiate API call from here
-        axios.get('https://jsonplaceholder.typicode.com/postss')
+        axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
                 console.log('Success data :', response.data);
-                this.setState( {Loading: false, news: response.data, error: null} );
+                this.setState( {Loading: false, users: response.data, error: null} );
             })
             .catch(error => {
                 const message = this.state.error? this.state.error['message'] : 
-                this.setState({Loading: false, news: null, error: error});
+                this.setState({Loading: false, users: null, error: error});
             })               
     }
         
@@ -46,19 +47,34 @@ export class Viji extends Component <IProps, IState> {
                 return errorJSX;
             }
         
-            renderNews( ) {
-                const newsJSX = <h4>List all news here..</h4>
-                return newsJSX;
+            renderUsersData( ) {
+                const users = this.state.users ? this.state.users : [];
+                const blogJSX = users.map( (items: {id: number, name: string, email: string}, index) => {
+                    return (
+                        <div key= {index + items.id} className ='ui red segment'>
+                            <div className="ui tertiary inverted segment">
+                            <span className="ui black text">
+                            <h5 key={items.id}>Name: {items.name}</h5>
+                            <h5 key={'#' + items.id}>Email: {items.email}</h5>
+                            </span>
+                            </div>
+                        </div>
+                    )
+                });
+                return blogJSX;
             }
         
             render( ) {
-                if ( this.state.Loading) {
-                    return this.renderLoading( );
-                } else if ( this.state.error) {
-                    return this.renderError( );
-                } else {
-                    return this.renderNews( );
-                }
+                return (
+                    <div>
+                        <h2 className="ui center aligned green header">{this.props.title}</h2>
+                        {
+                            this.state.Loading ? this.renderLoading( ) :
+                            this.state.users ? this.renderUsersData( ) :
+                            this.renderError( )    
+                        }
+                    </div>
+                )   
             }
    /* render( ) {
         return (
