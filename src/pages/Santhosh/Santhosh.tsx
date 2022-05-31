384fe02b1dc6c4283cd601f4b289e348cdddc111
Santhosh <ntasanthosh@gmail.com>
2022-05-31 14:30:35 +0530

    Added a Link to My Component Page

--- src/pages/Santhosh/Santhosh.tsx
+++ src/pages/Santhosh/Santhosh.tsx
@@ -1,49 +1,44 @@
 import { Component } from "react";
-import axios, { responseEncoding } from 'axios';
-import { Interface } from "readline";
-import { text } from "stream/consumers";
-import { table } from "console";
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+ 
 
 interface IState {
-    Loading: boolean, 
+    loading: boolean, 
     users: {} [] | null, 
     error: {message: string} | null;
-
 }
-
 interface IProps {
     title: String;
 }
 
-
 export class Santhosh extends Component<IProps, IState> {
 
-    state = {title: true, Loading: true, users: null, error: null };
+    state = { loading: true, users: null, error: null };
 
     componentDidMount( ) {
 
-        // Intitiate API call from here
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                this.setState( {Loading: false, users: response.data, error: null} );
+                this.setState( { loading: false, users: response.data, error: null } );
             })
             .catch(error => {
-                this.setState( {Loading: false, users: null, error: error} );
+                this.setState( { loading: false, users: null, error: error } );
             })                  
     }
     
     renderLoading( ) {
+
         const loadingJSX = 
         <div className="ui icon message">
             <i className="notched circle loading icon"></i>
             <h4>Loading Please wait.. </h4>
-        </div>
-         
+        </div>         
         return loadingJSX;
     }
 
     renderError( ) {
+
         const message = this.state.error? this.state.error['message'] : '';
         const errorJSX =
         <div>                  
@@ -51,39 +46,46 @@ export class Santhosh extends Component<IProps, IState> {
         </div> 
         return errorJSX;
     }
-   
- 
-    renderNews( ) {
-       
-        const users = this.state.users? this.state.users : [] ;  
-      
+    
+    renderNews( ) {      
+
+        const users = this.state.users? this.state.users : [] ;        
         const newsJSX = users.map( ( item: { id: number, name: string, email: any } )=> {            
             return (
                 <tr key={item.id + 'f'}>
-                    <td className="negative" width={"20px"} key={item.id + 'a'}>{item.id}</td>         
-                    <td className="negative" width={"200px"} key={item.id + 'b'}>{item.name}</td>                   
-                    <td className="negative" width={"200px"} key={item.id + 'c'}>{item.email}</td>                   
+                    <td className="negative" key={item.id + 'a'}>{item.id}</td>         
+                    <td className="negative" key={item.id + 'b'}>{item.name}</td>                   
+                    <td className="negative" key={item.id + 'c'}>{item.email}</td>                   
                 </tr>
             );
         }); 
-
         return newsJSX;
     }
  
     render( ) {        
            
         return (
-        <div>              
-        <h3 className="ui center aligned header">{ this.props.title }</h3>                  
-        {
-            this.state.Loading? this.renderLoading( ) :              
-            this.state.users? <><table className="ui celled structured table">
-            <thead><tr><td width={"20px"} ><h4>ID</h4></td><td width={"200px"}><h4>Name</h4></td><td width={"200px"}><h4>Email</h4></td></tr></thead>
-            <tbody>{ this.renderNews( )}</tbody></table> </> : 
-            this.renderError( )
-        }  
-        </div>
-        )
-        
+            <div>              
+                <h3 className="ui center aligned red header">{ this.props.title }</h3>    
+                <br/>
+                <Link to='/home' className="ui orange button" >Home</Link>
+                <br/>              
+                {
+                    this.state.loading? this.renderLoading( ) :              
+                    this.state.users? <>
+                    <table className="ui celled table">
+                        <thead>
+                            <tr>
+                                <td className="orange" align="center" ><h4>ID</h4></td>
+                                <td className="orange" align="center" ><h4>Name</h4></td>
+                                <td className="orange" align="center" ><h4>Email</h4></td>
+                            </tr>
+                        </thead>
+                        <tbody>{ this.renderNews( ) }</tbody>
+                    </table> </> : 
+                    this.renderError( )
+                }  
+            </div>
+        )        
     }
 } 
