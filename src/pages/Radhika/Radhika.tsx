25eb35ef1f6418d5e80841c3eae2601ef1574464
AnusreeMohan <anusreemohan@gmail.com>
2022-05-31 14:52:47 +0530

    spacing issues resolved

--- src/pages/Radhika/Radhika.tsx
+++ src/pages/Radhika/Radhika.tsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 
 interface IState {
     loading: boolean,
-    users: {}[] | null,
-    error: {message: string} | null, 
+    users: { } [ ] | null,
+    error: { message: string } | null, 
 }
+
 interface IProps {
     title: string;
 }
@@ -17,50 +18,43 @@ export class Radhika extends Component <IProps, IState> {
     state= { loading: true, users: null, error: null };
 
     componentDidMount( ) {
+
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
-            this.setState({loading: false, users: response.data, error: null});
+            this.setState( {loading: false, users: response.data, error: null} );
         })
         .catch(error => {
-            this.setState({loading: false, users: null, error: error});
+            this.setState( {loading: false, users: null, error: error} );
         })
     }
-
-    componentDidUpdate( ) {
-        //console.log('componentDidUpdate')
-     }
- 
-     componentWillUnmount( ) {
-        //console.log('componentWillUnmount')
-     }
      
     renderLoading( ) {
         const loadingJSX = 
         <div>
-            <i className="notched circle loading icon"></i>
-            <div className="content">
-                <h4> Loading ....</h4>
+            <i className = "notched circle loading icon"></i>
+            <div className = "content">
+                <h4>Loading ....</h4>
             </div>
         </div> 
         return loadingJSX;
     }
 
     renderError( ) {
-        const message= this.state.error? this.state.error['message'] : '';
+        const message = this.state.error ? this.state.error[ 'message' ] : '';
         const errorJSX = 
-                <div className="ui red message">
-                    <h4>{message}</h4>
+                <div className = "ui red message">
+                    <h4>{ message }</h4>
                 </div>
         return errorJSX;
     }
 
     renderUserdata( ) {
-        const users = this.state.users ? this.state.users : [];
-        const dataJSX = users.map(  ( user: {id: number, name: string, phone: number} ) => {
+        const users = this.state.users ? this.state.users : [ ];
+        const dataJSX = users.map( ( user: {id: number, name: string, phone: number} ) => {
             return (
-                <div key= {user.id} className="ui blue segment">
-                    <p><b>Name:</b> {user.name}</p>
-                    <p><b>Phone:</b> {user.phone}</p>
+                <div key = { user.id } className="ui blue segment">
+                    <p><b>Name:</b>{ user.name }</p>
+                    <p><b>Phone:</b>{ user.phone }</p>
                 </div>
             );
         });
@@ -70,13 +64,14 @@ export class Radhika extends Component <IProps, IState> {
     render( ) {
         return(
             <div>
-                <h2 className="ui center aligned header">{ this.props.title }</h2>
+                <h2 className = "ui center aligned header">{ this.props.title }</h2>
                 <br/>
-                <Link to='/' className="item">Go to Home Page</Link>
+                <Link to='/' className = "item">Go to Home Page</Link>
                 <br/>
                 {
-                    this.state.loading? this.renderLoading( ) : 
-                    this.state.users? <><h3 className="ui center aligned header">User Details</h3>{ this.renderUserdata( ) } </> : <> { this.renderError( ) } </>
+                    this.state.loading ? this.renderLoading( ): 
+                    this.state.users ? <><h3 className="ui center aligned header">User Details</h3>{ this.renderUserdata( ) }</>:
+                    this.renderError( )
                 }
             </div>
         );
--- src/pages/Santhosh/Santhosh.tsx
+++ src/pages/Santhosh/Santhosh.tsx
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 
 interface IState {
     loading: boolean, 
-    users: {} [] | null, 
-    error: {message: string} | null;
+    users: { } [ ] | null, 
+    error: { message: string } | null;
 }
+
 interface IProps {
     title: String;
 }
@@ -20,18 +21,18 @@ export class Santhosh extends Component<IProps, IState> {
 
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                this.setState( { loading: false, users: response.data, error: null } );
+                this.setState( {loading: false, users: response.data, error: null} );
             })
             .catch(error => {
-                this.setState( { loading: false, users: null, error: error } );
+                this.setState( {loading: false, users: null, error: error} );
             })                  
     }
     
     renderLoading( ) {
 
         const loadingJSX = 
-        <div className="ui icon message">
-            <i className="notched circle loading icon"></i>
+        <div className = "ui icon message">
+            <i className =" notched circle loading icon"></i>
             <h4>Loading Please wait.. </h4>
         </div>         
         return loadingJSX;
@@ -39,23 +40,23 @@ export class Santhosh extends Component<IProps, IState> {
 
     renderError( ) {
 
-        const message = this.state.error? this.state.error['message'] : '';
+        const message = this.state.error? this.state.error[ 'message' ] : '';
         const errorJSX =
         <div>                  
-            <h3 className="ui red message">{ message }</h3>
+            <h3 className = "ui red message">{ message }</h3>
         </div> 
         return errorJSX;
     }
     
     renderNews( ) {      
 
-        const users = this.state.users? this.state.users : [] ;        
+        const users = this.state.users? this.state.users : [ ] ;        
         const newsJSX = users.map( ( item: { id: number, name: string, email: any } )=> {            
             return (
-                <tr key={item.id + 'f'}>
-                    <td className="negative" key={item.id + 'a'}>{item.id}</td>         
-                    <td className="negative" key={item.id + 'b'}>{item.name}</td>                   
-                    <td className="negative" key={item.id + 'c'}>{item.email}</td>                   
+                <tr key={ item.id }> 
+                    <td className = "negative">{ item.id }</td>         
+                    <td className = "negative">{ item.name }</td>                   
+                    <td className = "negative">{ item.email }</td>                   
                 </tr>
             );
         }); 
@@ -66,19 +67,19 @@ export class Santhosh extends Component<IProps, IState> {
            
         return (
             <div>              
-                <h3 className="ui center aligned red header">{ this.props.title }</h3>    
+                <h3 className = "ui center aligned red header">{ this.props.title }</h3>    
                 <br/>
-                <Link to='/home' className="ui orange button" >Home</Link>
+                <Link to='/home' className = "ui orange button" >Home</Link>
                 <br/>              
                 {
-                    this.state.loading? this.renderLoading( ) :              
-                    this.state.users? <>
+                    this.state.loading ? this.renderLoading( ) :              
+                    this.state.users ? <>
                     <table className="ui celled table">
                         <thead>
                             <tr>
-                                <td className="orange" align="center" ><h4>ID</h4></td>
-                                <td className="orange" align="center" ><h4>Name</h4></td>
-                                <td className="orange" align="center" ><h4>Email</h4></td>
+                                <td className = "orange" align = "center" ><h4>ID</h4></td>
+                                <td className = "orange" align = "center" ><h4>Name</h4></td>
+                                <td className =" orange" align = "center" ><h4>Email</h4></td>
                             </tr>
                         </thead>
                         <tbody>{ this.renderNews( ) }</tbody>
