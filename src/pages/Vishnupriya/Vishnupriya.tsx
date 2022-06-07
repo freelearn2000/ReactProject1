b7c83dc502e4b03b5ffad1917a32d47f716b148e
location <priya.piya2008@gmail.com>
2022-06-07 15:23:56 +0530

    feat: Added Hoc with component wrapped

--- src/pages/Vishnupriya/Vishnupriya.tsx
+++ src/pages/Vishnupriya/Vishnupriya.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { retriveDataFromRoute } from '../../utils/hoc';
 
 
 interface IProps {
@@ -9,22 +10,22 @@ interface IProps {
 
 interface IState {
     loading: boolean;
-    blogs: { } [ ] | null;
+    users: { } [ ] | null;
     error: { message: string } | null;
 }
 
-export class Vishnupriya extends Component<IProps, IState> {
+class Vishnupriya extends Component<IProps, IState> {
 
-    state = { loading: true, blogs: null, error: null };
+    state = { loading: true, users: null, error: null };
 
     componentDidMount( ) {
 
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                this.setState( {loading: false, blogs: response.data, error: null} );
+                this.setState( {loading: false, users: response.data, error: null} );
             })
             .catch(error => {
-                this.setState( {loading: false, blogs: null, error: error} );
+                this.setState( {loading: false, users: null, error: error} );
             })
     }
 
@@ -33,10 +34,10 @@ export class Vishnupriya extends Component<IProps, IState> {
         const loadingJSX = 
             <div>
                 <h2>{ this.props.title }</h2>
-                <div className = "ui segment">
+                <div className="ui segment">
                     <p>Loading...</p>
-                    <div className = "ui active dimmer">
-                        <div className = "ui loader">Please wait...</div>
+                    <div className="ui active dimmer">
+                        <div className="ui loader">Please wait...</div>
                     </div>
                 </div>
             </div>
@@ -48,22 +49,22 @@ export class Vishnupriya extends Component<IProps, IState> {
         const message = this.state.error ? this.state.error[ `message` ] : '';
         const errorJSX =
             <div>
-                <div className = "ui negative message">
-                <i className = "close icon"></i>
+                <div className="ui negative message">
+                <i className="close icon"></i>
                     { message }
                 </div>
             </div>
         return errorJSX;
     }
 
-    renderBlogData( ) {
+    renderUserData( ) {
 
-        const blogs = this.state.blogs ? this.state.blogs : [ ];
-        const blogJSX = blogs.map( (items: {id: number, name: string, email: string}) => {
+        const blogs = this.state.users ? this.state.users : [ ];
+        const blogJSX = blogs.map( ( user: {id: number, name: string, email: string} ) => {
             return (
-                <div key= { items.id } className = 'ui two segment'>
-                    <h5>Name: { items.name }</h5>
-                    <p>Email: { items.email }</p>
+                <div key={ user.id } className="ui two segment">
+                    <h5>Name: { user.name }</h5>
+                    <p>Email: { user.email }</p>
                 </div>
             )
         });
@@ -74,17 +75,19 @@ export class Vishnupriya extends Component<IProps, IState> {
 
         return (
             <div>
-                <h2 className = "ui center aligned blue header">{this.props.title}</h2>
+                <h2 className="ui center aligned blue header message">{this.props.title}</h2>
                 <br/>
-                <button className = "ui button"><Link to='/' className="item">Home</Link></button>
-                <button className = "ui button"><Link to='/about/abc' className="item">About</Link></button>
+                <button className="ui button"><Link to='/' className="item">Home</Link></button>
+                <button className="ui button"><Link to='/news/popular' className="item">News</Link></button>
                 <br/>
                 {
                     this.state.loading ? this.renderLoading( ) :
-                    this.state.blogs ? this.renderBlogData( ) :
+                    this.state.users ? this.renderUserData( ) :
                     this.renderError( )    
                 }
             </div>
         )   
     }
 }
+
+export default retriveDataFromRoute( Vishnupriya );
