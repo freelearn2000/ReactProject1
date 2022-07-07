1732cf028b973dfb7fe19e5337600c709c2e3b8f
Santhosh <ntasanthosh@gmail.com>
2022-07-07 11:28:55 +0530

    Implemented Context

---
+++ src/pages/Santhosh/Abc.tsx
@@ -0,0 +1,67 @@
+import { Component, createContext, useContext } from "react";
+
+
+// Create Context object
+const mycontext = createContext('');
+
+// Provider
+export class Abc extends Component {
+    
+    render( ) {
+        return (
+            <>
+            <h1>Context</h1>
+                <mycontext.Provider value={'Santhosh'}>
+                     <Middle/>
+                </mycontext.Provider>
+            </>
+        );        
+    }
+}
+
+class Middle extends Component {
+
+    render( ) {
+        return (
+            <Child3/>
+        );
+    }
+}
+
+// a. Consumer
+class Child1 extends Component {
+
+    render( ) {
+        return (
+            <mycontext.Consumer>
+                { value =>
+                    (
+                        <> <h1> Context value accessed from child1 : {value} </h1></>
+                    )
+                }
+            </mycontext.Consumer>
+        );
+    }
+}
+
+// b. Consumer
+class Child2 extends Component {
+
+    static contextType = mycontext;
+
+    render( ) {
+        return (
+            <>Context value accessed from child2 : {this.context};</>
+        );
+    }
+}
+
+// c. Consumer
+const Child3 = (props: any) =>{
+
+    const context = useContext(mycontext);
+
+    return (
+        <>Context value accessed from child3 : {context}</>
+    );
+}
--- src/pages/Santhosh/Home.tsx
+++ src/pages/Santhosh/Home.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 
 export const Home = ( props: any ) => {  
 
     return (
         <div>
-            <h3>Nested Routing Home Page</h3>  
+            <h3>Welcome to Home Page</h3>  
             <div className="ui buttons">
                 <button className="ui blue button">Cancel</button>
-                <div className="or"></div>
-                <button className="ui positive button" >Continue....</button>
+                <div className="or"></div>                             
+                <Link to='/Santhosh/Abc'  className="ui green button">Continue....</Link> 
             </div>
         </div>
     );
