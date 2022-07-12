49339bfb9306ba7e1935480e8c49aeb3b079a36f
Radhika <radhu.ps@gmail.com>
2022-07-12 12:34:53 +0530

    Implemented context

--- src/pages/Radhika/Entertainment.tsx
+++ src/pages/Radhika/Entertainment.tsx
@@ -1,18 +1,73 @@
-import { Component } from 'react';
+import { Component, createContext, useContext } from 'react';
 
-interface IProps {
-    title: string;   
-}
+// Create Context object
+const MyContext = createContext('');
 
 
-export class Entertainment extends Component<IProps> {
+export class Entertainment extends Component { 
 
     render( ) {
-
         return (
             <div>
-                <h2 className="ui center aligned green header message">{ this.props.title }</h2>
+                <h2 className="ui center aligned green header message"> { 
+                    <MyContext.Provider value={'News'}>
+                        <Music/>
+                    </MyContext.Provider>
+                }
+                </h2>
             </div>
         );
     }
 }
+
+class Music extends Component {
+
+    render ( ) {
+        return (
+            <div>
+                <Classical/>
+            </div>
+        );
+    }
+}
+class Classical extends Component {
+
+    render ( ) {
+        return ( 
+                <div>
+                    <MyContext.Consumer>
+                    { value =>
+                        (
+                            <> Latest Entertainment { value } </>
+                        )
+                    }
+                    </MyContext.Consumer>
+                </div>
+        );
+    }
+}
+
+/*
+class Hindustani extends Component {
+
+    static contextType = MyContext;
+
+    render( ) {
+         return (
+            <>Play the music from Hindustani : {this.context};</>
+         );
+    }
+}
+
+
+  Consumer
+
+const Ghazal = (props: any) =>{
+
+    const context = useContext(mycontext);
+
+    return (
+        <>Music played : {context}</>
+    );
+}
+*/
