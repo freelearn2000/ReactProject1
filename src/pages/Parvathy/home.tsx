fde9b995083c825460fbe7e955bcb8d02a89566b
Parvathy Hari <parvathyvhari@gmail.com>
2022-07-12 12:14:26 +0530

    implemented context

--- src/pages/Parvathy/home.tsx
+++ src/pages/Parvathy/home.tsx
@@ -1,16 +1,23 @@
-import { Component } from "react";
+import { Component, createContext } from "react";
 import picture from "./pictures/catimg1.png";
 
 
+// Create context object
+const MyContext = createContext('');
+
+// Provider
 class Home extends Component {
 
     render() {
 
         return (
             <div>
+                <MyContext.Provider value={'Visualization'}>
+                    <Middle />
+                </MyContext.Provider>
                 <h2 className="ui center aligned header">A room without books is like a body without a soul.</h2>
                 <div className="ui image">
-                    <img alt={picture} src={picture}/>
+                    <img alt={ picture } src={ picture } />
                 </div>
             </div>
         )
@@ -18,3 +25,27 @@ class Home extends Component {
 }
 
 export default (Home);
+
+export class Middle extends Component {
+
+    render() {
+        return (
+            <Child />
+        );
+    }
+}
+
+export class Child extends Component {
+
+    render() {
+        return (
+            <MyContext.Consumer>
+                {
+                    value => (
+                        <h3> Context value accessed from Child : { value } </h3>
+                    )
+                }
+            </MyContext.Consumer>
+        );
+    }
+}
