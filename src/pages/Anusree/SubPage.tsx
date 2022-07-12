8a782f3dc9acf38be5ff1d9e33d4dec19365b15e
AnusreeMohan <anusreemohan@gmail.com>
2022-07-12 12:22:55 +0530

    added context objects

--- src/pages/Anusree/SubPage.tsx
+++ src/pages/Anusree/SubPage.tsx
@@ -1,10 +1,14 @@
-import { Component } from "react";
+import { Component, createContext, useContext } from "react";
 import { retriveDataFromRoute } from '../../utils/hoc';
 
 interface IProps {
     title: any;
     location: any;
 }
+
+//Create Content object
+const NewContext = createContext('');
+const NewTheme = createContext('dark');
 
 class SubPage extends Component<IProps> {
     search = this.props.location.search;
@@ -15,14 +19,75 @@ class SubPage extends Component<IProps> {
     render( ) {
 
         return(
-            
             <div className="ui segment">
-                 
                 <h2 className="ui center aligned header">{ this.props.title }</h2>
-               <p className="paragraph">{this.contents}</p>
+                <p className="paragraph">{ this.contents }</p>
+                <NewContext.Provider value={'Anu'}>
+                    <Child1/>
+                </NewContext.Provider>
             </div>
-        )
+        );
     }
+}
+
+class Child1 extends Component {
+
+    render( ) {
+
+        return( 
+            <Child2/> 
+        );
+    }
+}
+
+class Child2 extends Component {
+
+    render( ) {
+
+        return(
+            <>
+            <Child3/>
+            </>
+        );
+    }
+}
+
+class Child3 extends Component {
+
+    render( ) {
+        return(
+            <>
+            <NewContext.Consumer>
+                {
+                    value => (<>Context value accessed from child3:{value}</>)
+                }
+            
+            </NewContext.Consumer>
+            
+            <Child4/> 
+            </>
+        );
+    }
+}
+
+class Child4 extends Component {
+    static contextType = NewContext;
+    render( ) {
+        return(
+            <>
+            Context value accessed from child4 : {this.context}
+            <Child5/>
+            </>
+            
+        );
+    }
+}
+
+const Child5 = ( ) => {
+    const context = useContext(NewTheme);
+    return (
+        <>  Context value accessed from child5 : {context}</>
+    )
 }
 
 export default retriveDataFromRoute(SubPage);
