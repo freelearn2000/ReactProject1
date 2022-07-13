f4f5422c50c0078a901cb88d3c62ec57ce4fbda0
Divyao <94785328+Divyao@users.noreply.github.com>
2022-07-13 14:33:01 +0530

    feat:implemented context

--- src/context/global.ts
+++ src/context/global.ts
@@ -6,6 +6,8 @@ export const CounterContext = createContext(0);
 
 export const UserContext = createContext({name:'Guest', viewMode:'Guest'});
 
+export const User = createContext({name:'Guest', userRole:'Guest'});
+
 export const ContextWeather = createContext('');
 
 export const MyMusic = createContext('');
