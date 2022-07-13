fd2f20fc847759d366b8dac37be6d941502856e2
Bindu <kumaribindur@gmail.com>
2022-07-13 14:09:36 +0530

    Implemented  context globally

--- src/context/global.ts
+++ src/context/global.ts
@@ -1,17 +1,15 @@
 import { createContext } from "react";
+import { start } from "repl";
 
 
 export const CounterContext = createContext(0);
-export const User = createContext({name:'Guest', userRole:'Guest'})
+
 export const UserContext = createContext({name:'Guest', viewMode:'Guest'});
 
 export const ContextWeather = createContext('');
 
 export const MyMusic = createContext('');
 
-<<<<<<< HEAD
 export const  SportsContext = createContext('');
 
-=======
->>>>>>> c1796e38fb404c4ccc6d029eeae30b91ad197e2e
 export const PetsWorldContext = createContext('1');
